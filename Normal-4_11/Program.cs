a24ee4de5ac64f31c985cc0fe9b22221adecb31a
Vasya Bogatchuk <v380962437840@gmail.com>
2020-08-16 00:35:11 +0300

    no message

---
+++ Normal-4_11/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Normal_4_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = 0;
+            Console.Write("Введите размер массива-> ");
+            size = Convert.ToInt32(Console.ReadLine());
+            int[] number = new int[size];
+
+            Random r = new Random();
+            for (int i = 0; i < number.Length; i++)
+            {
+                number[i] = r.Next(-100, 100);
+                Console.Write(number[i] + " ");
+            }
+            for (int i = 0; i < number.Length; i++)
+            {
+                if (number[i] % 2 == 0)
+                {
+                    Console.Write("\nЧетные числа :{0} ", number[i]);
+                }
+            }
+            for (int i = 0; i < number.Length; i++)
+            {
+                if (number[i] % 2 != 0)
+                {
+                    Console.Write("\nНе четные числа: {0} ", number[i]);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
